bfe8a75712c4178d5d67931702cc8709926dc2d7
Jesus Gaspar <jgaspar.devops@gmail.com>
2022-05-23 12:46:48 -0400

    [ADD] class numbers

---
+++ src/04-numbers.ts
@@ -0,0 +1,22 @@
+(() => {
+  let productPrice = 100
+  productPrice = 12
+  console.log(productPrice)
+
+  let customerAge: number = 34
+  customerAge = customerAge + 1
+
+  let productInStock: number
+  console.log('productInStock', productInStock)
+  if(productInStock > 10){
+    console.log('isGreater')
+  }
+
+  let discount = parseInt('gfdsgsdfgsdfg')
+  console.log(discount)
+
+  let hex = 0xfff
+  console.log('hex', hex)
+  let bin = 0b10101010
+  console.log('bin', bin)
+})()
