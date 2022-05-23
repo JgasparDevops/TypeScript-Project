9924f7139f18c85b0261ca78faaa3b08a2232751
Jesus Gaspar <jgaspar.devops@gmail.com>
2022-05-23 13:30:20 -0400

    [ADD] class any

--- src/08-any.ts
+++ src/08-any.ts
@@ -4,3 +4,10 @@ myDynamicVar = null
 myDynamicVar = {
 }
 myDynamicVar = 'string'
+
+const rta = (myDynamicVar as string).toLowerCase()
+console.log(rta)
+
+myDynamicVar = 1212
+const rta1 = (<number>myDynamicVar).toFixed()
+console.log(rta1)
